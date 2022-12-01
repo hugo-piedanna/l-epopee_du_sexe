7d809d12b966eed174f67d4d83353fbb926048e0
Hugo Piedanna <h.piedanna@gmail.com>
2022-12-01 21:17:39 +0100

    Animation

--- js/motion.js
+++ js/motion.js
@@ -3,26 +3,42 @@ const directions = {
     left: "left"
 }
 
+var animationInterval;
+var spriteSheet = document.getElementById("avatar");
+var widthOfSpriteSheet = 1472;
+var widthOfEachSprite = 184;
 let myDirection = directions.right;
 
-function forward(){
-    console.log("Current direction: " + myDirection)
-}
-
-function equalsIgnoringCase(text, other) {
-    return text.localeCompare(other, undefined, { sensitivity: 'accent' }) === 0;
-}
+function forward() {
+    var position = widthOfEachSprite; //start position for the image
+    const speed = 100; //in millisecond(ms)
+    const diff = widthOfEachSprite; //difference between two sprites
+  
+    animationInterval = setInterval(() => {
+      spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+  
+      if (position < widthOfSpriteSheet) {
+        position = position + diff;
+      } else {
+        //increment the position by the width of each sprite each time
+        position = widthOfEachSprite;
+      }
+      //reset the position to show first sprite after the last one
+    }, speed);
+  }
 
 document.body.addEventListener("keydown", (e) => {
     switch(e.key){
         case "ArrowRight":
-        case equalsIgnoringCase("d", "D"):
+        case "D":
+        case "d":
             if(myDirection != directions.right){
                 myDirection = directions.right;
             }
             break;
         case "ArrowLeft":
-        case equalsIgnoringCase("q", "Q"):
+        case "Q":
+        case "q":
             if(myDirection != directions.left){
                 myDirection = directions.left;
             }
